77e932c9a12090a1386e2a040cea40b8b1cf22d2
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-02-11 12:14:24 +0100

    Refactor CLI tests

--- __test__/bin/index.test.js
+++ __test__/bin/index.test.js
@@ -1,20 +1,32 @@
-const { execSync } = require('child_process')
+const { exec } = require('child_process')
 const path = require('path')
 
 const CliFile = () => path.join(process.cwd(), 'bin/check-my-headers.js')
 describe('CLI Behaviour', () => {
-  test('Success process: external url', () => {
-    const exe = execSync(`node ${CliFile()} http://github.com/ulsiesgascon/check-my-headers`)
-    expect(exe.toString()).toMatchSnapshot()
+  test('Success process: external url', (done) => {
+    exec(`node ${CliFile()} http://github.com/ulsiesgascon/check-my-headers`, (err, stdout, stderr) => {
+      expect(err).toBe(null)
+      expect(stdout).toMatchSnapshot()
+      expect(stderr).toMatchSnapshot()
+      done()
+    })
   })
 
-  test('Success process: localhost url', () => {
-    const exe = execSync(`node ${CliFile()} localhost:3000`)
-    expect(exe.toString()).toMatchSnapshot()
+  test('Success process: localhost url', (done) => {
+    exec(`node ${CliFile()} localhost:3000`, (err, stdout, stderr) => {
+      expect(err).toBe(null)
+      expect(stdout).toMatchSnapshot()
+      expect(stderr).toMatchSnapshot()
+      done()
+    })
   })
 
-  test('Error process: Missing URL', () => {
-    const exe = execSync(`echo ${CliFile()}`)
-    expect(exe.toString()).toMatchSnapshot()
+  test('Error process: Missing URL', (done) => {
+    exec(`node ${CliFile()}`, (err, stdout, stderr) => {
+      expect(err.message).toMatch('Missing argument URL!')
+      expect(stdout).toMatchSnapshot()
+      expect(stderr).toMatchSnapshot()
+      done()
+    })
   })
 })
