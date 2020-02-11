2fe9510e2c1b0f0d7af9930bcf22beb3f2a89558
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-02-11 18:08:19 +0100

    Improved CLI UX
    - Added emojis
    - Added support for promises

--- bin/check-my-headers.js
+++ bin/check-my-headers.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 
-'use strict'
+(async () => {
+  const checkMyHeaders = require('../src/index.js')
+  const { printError, printInfo } = require('./helpers')
+  const pkg = require('../package.json')
 
-const checkMyHeaders = require('../src/index.js')
-const { printError, printInfo } = require('./helpers')
-const pkg = require('../package.json')
+  printInfo(`👋  Welcome to ${pkg.name}@${pkg.version}`)
 
-printInfo(`Welcome to ${pkg.name}@${pkg.version}`)
+  const url = process.argv[2]
+  if (!url) {
+    printError('😬  Missing argument URL!')
+    printInfo('👉  Example: check-my-headers https://github.com/ulisesgascon/check-my-headers')
+    process.exit(1)
+  }
 
-const url = process.argv[2]
-if (!url) {
-  printError('Missing argument URL!')
-  printInfo('Example: check-my-headers https://github.com/ulisesgascon/check-my-headers')
-  process.exit(1)
-}
+  printInfo(`🚀  The analysis has started for ${url}...`)
 
-printInfo('The analysis has started...')
+  await checkMyHeaders(url)
 
-checkMyHeaders(url)
+  printInfo('😄  Thanks for using check-my-headers!')
+})()
